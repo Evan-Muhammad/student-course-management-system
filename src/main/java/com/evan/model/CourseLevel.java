25ecf0db2cee1ce969675cc01bfed650ad2d0e28
Evan Muhammad <evanmuhammad107@gmail.com>
2026-07-11 18:56:53 +0300

    Add Department and CourseLevel enums

---
+++ src/main/java/com/evan/model/CourseLevel.java
@@ -0,0 +1,7 @@
+package com.evan.model;
+
+public enum CourseLevel {
+    BEGINNER,
+    INTERMEDIATE,
+    ADVANCED
+}
---
+++ src/main/java/com/evan/model/Department.java
@@ -0,0 +1,9 @@
+package com.evan.model;
+
+public enum Department {
+    COMPUTER_SCIENCE,
+    MATHEMATICS,
+    PHYSICS,
+    BUSINESS,
+    ENGINEERING
+}
